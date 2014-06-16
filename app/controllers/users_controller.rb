4fd353a4456d5bb2d839124b72ff6ff497fb6bb7
tanamako <tanaka_makoto@cyberagent.co.jp>
2014-06-16 14:29:49 +0900

    user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Monest"
+      flash[:success] = "Monesta"
       redirect_to @user
     else
       render 'new'
